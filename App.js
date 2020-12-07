1d0b079fce3e36c9947455df3135d2f68c8f2cc4
Boey Kok Chin <boeykokchin@live.com.sg>
2020-12-08 01:26:37 +0800

    based - part 1 up till slide 90

--- App.js
+++ App.js
@@ -3,13 +3,22 @@ import { StyleSheet, Text, View } from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
 import BlockRGB from './components/BlockRGB';
+import { FlatList } from 'react-native-gesture-handler';
 
 function HomeScreen() {
+  const [colorArray, setColorArray] = useState([
+    { red: 255, green: 0, blue: 0, id: '0' },
+    { red: 0, green: 255, blue: 0, id: '1' },
+    { red: 0, green: 0, blue: 255, id: '2' },
+  ]);
+
+  function renderItem({ item }) {
+    return <BlockRGB red={item.red} green={item.green} blue={item.blue} />;
+  }
+
   return (
-    <View style={styles.container}>
-      <BlockRGB red={255} green={0} blue={0} />
-      <BlockRGB red={0} green={255} blue={0} />
-      <BlockRGB red={0} green={0} blue={255} />
+    <View>
+      <FlatList data={colorArray} renderItem={renderItem} />
     </View>
   );
 }
